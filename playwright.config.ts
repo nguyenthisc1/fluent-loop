4da0041d120eb0825bfe747567c49c2d63bdae6f
Thi Nguyễn <nguyenthisc1@gmail.com>
2026-05-12 20:22:24 +0700

    feat: add end-to-end testing setup with Playwright and update package dependencies

---
+++ playwright.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./src/features/user/__test__/e2e",
+  timeout: 30_000,
+  use: {
+    baseURL: "http://localhost:5173",
+    trace: "on-first-retry",
+  },
+  webServer: {
+    command: "pnpm dev",
+    url: "http://localhost:5173",
+    reuseExistingServer: true,
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+  ],
+});
