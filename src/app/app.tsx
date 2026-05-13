c75bec132062042792d061c082a0625f834cb31e
Thi Nguyễn <nguyenthisc1@gmail.com>
2026-05-13 17:14:45 +0700

    chore: update Tailwind CSS configuration and enhance styling with Fluent design tokens

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,6 @@
 import { RouterProvider } from "react-router-dom";
-import "../App.css";
-import { router } from "./router";
 import { AppProviders } from "./provider";
+import { router } from "./router";
 
 function App() {
   return (
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,mdx}"],
+
+  theme: {},
+  plugins: [
+    // require('@tailwindcss/forms'),
+    // require('@tailwindcss/typography'),
+  ],
 };
