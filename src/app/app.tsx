8e1cbdc08af8afc08e03ed6181438332cb731f63
Thi Nguyễn <nguyenthisc1@gmail.com>
2026-05-13 13:54:40 +0700

    refactor: simplify App component structure by introducing AppProviders

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,19 +1,14 @@
-import { UserDependenciesProvider } from "@/features/user/presentation/providers/user-dependencies-provider";
-import { UserProvider } from "@/features/user/presentation/providers/user-providers";
-import { createUserDependencies } from "@/features/user/user.container";
 import { RouterProvider } from "react-router-dom";
 import "../App.css";
 import { router } from "./router";
+import { AppProviders } from "./provider";
 
 function App() {
-  const userDependencies = createUserDependencies();
   return (
     <>
-      <UserDependenciesProvider dependencies={userDependencies}>
-        <UserProvider>
-          <RouterProvider router={router} />
-        </UserProvider>
-      </UserDependenciesProvider>
+      <AppProviders>
+        <RouterProvider router={router} />
+      </AppProviders>
     </>
   );
 }
--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,17 +1,26 @@
+import { UserDependenciesProvider } from "@/features/user/presentation/providers/user-dependencies-provider";
+import { UserProvider } from "@/features/user/presentation/providers/user-providers";
+import { createUserDependencies } from "@/features/user/user.container";
 import { QueryClientProvider } from "@tanstack/react-query";
+import type { ReactNode } from "react";
 import { Toaster } from "sonner";
 import { queryClient } from "../shared/lib/query-client";
-import type { ReactNode } from "react";
 
 type AppProvidersProps = {
   children: ReactNode;
 };
 
 export function AppProviders({ children }: AppProvidersProps) {
+  const userDependencies = createUserDependencies();
+
   return (
     <QueryClientProvider client={queryClient}>
-      {children}
-      <Toaster richColors position="top-right" />
+      <UserDependenciesProvider dependencies={userDependencies}>
+        <UserProvider>
+          {children}
+          <Toaster richColors position="top-right" />
+        </UserProvider>
+      </UserDependenciesProvider>
     </QueryClientProvider>
   );
 }
